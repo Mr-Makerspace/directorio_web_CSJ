525d955ff7ba864784d0f523a9934db74465ef43
Mr-Makerspace <j.abarca@cumbressanjavier.edu.mx>
2025-10-15 14:32:50 -0600

    Update firebase-config.js
    Se remueve los import de firebase

--- firebase-config.js
+++ firebase-config.js
@@ -1,9 +1,3 @@
-  import { initializeApp } from "https://www.gstatic.com/firebasejs/12.4.0/firebase-app.js";
-  import { getAnalytics } from "https://www.gstatic.com/firebasejs/12.4.0/firebase-analytics.js";
-  // TODO: Add SDKs for Firebase products that you want to use
-  // https://firebase.google.com/docs/web/setup#available-libraries
-  // Your web app's Firebase configuration
-  // For Firebase JS SDK v7.20.0 and later, measurementId is optional
   const firebaseConfig = {
     apiKey: "AIzaSyD3F1GCQDtEiAGUyprhDwIbuXLpb5Wm31s",
     authDomain: "directorio-web-csj.firebaseapp.com",
